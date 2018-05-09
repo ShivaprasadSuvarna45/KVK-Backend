e6f1c44304116d84734f8e48956b36afcdfcd46e
ShivaprasadSuvarna45 <shivaprasad438@gmail.com>
2018-05-09 16:43:10 +0530

    database url

--- server/models/index.js
+++ server/models/index.js
@@ -9,10 +9,11 @@ const db = {};
 let sequelize;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize('postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm'
+  );
 } else {
   sequelize = new Sequelize(
-    config.database, config.username, config.password, config
+    'postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm'
   );
 }
 
