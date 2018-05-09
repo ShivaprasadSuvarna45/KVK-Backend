dc7dbfa50444e5f20c3b6c9d6208f45036c52e12
ShivaprasadSuvarna45 <shivaprasad438@gmail.com>
2018-05-09 16:55:05 +0530

    database url for connecting to db. Model configuration in routes.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const userController = require('../controllers').User;
-const userModal = require('../models/user');
+const userModal = require('../models').User;
 const generator = require('generate-password');
 const nodemailer = require('nodemailer');
 
