d17b511ab7033ffe6fe386d4da04ccc40e8fe962
ShivaprasadSuvarna45 <shivaprasad438@gmail.com>
2018-05-09 13:55:23 +0530

    change

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = "development";
+const env = "production";
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
