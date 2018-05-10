7b14bba8b43abb9dc2a477b419068833691a5e42
ShivaprasadSuvarna45 <shivaprasad438@gmail.com>
2018-05-10 07:20:52 +0530

    chek the fixes

--- server/models/index.js
+++ server/models/index.js
@@ -2,19 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'production';
+const env = 'production';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  //sequelize = new Sequelize('postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm');
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize('postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm');
+  //sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  //sequelize = new Sequelize('postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm');
-  sequelize = new Sequelize(
-    config.database, config.username, config.password, config
-  );
+  sequelize = new Sequelize('postgres://igbowcdjeurhgd:e896caf072dfa8685ba083ac72e5352570368cd0bf2ee383eded31a05b47d931@ec2-23-23-180-121.compute-1.amazonaws.com:5432/d2k6ajc030e3lm');
+  //sequelize = new Sequelize(
+  //  config.database, config.username, config.password, config
+  //);
 }
 
 
